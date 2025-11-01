8653ab5b78fc72e6bca7ad4f31ed120673269aeb
alexandre1830 <alexandre.olv.sz@gmail.com>
2025-11-01 15:55:09 -0300

    Week 1 Exercises

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade in percentage? ");
+        int grade = int.Parse(Console.ReadLine());
+        string letter;
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is: {letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You have passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately, you have not passed the course. Better luck next time!");
+        }
     }
 }
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        Console.Write("Guess a number between 1 and 100 (enter an integer): ");
+        int guess = int.Parse(Console.ReadLine());
+
+        while (guess != number)
+        {
+            if (guess < number)
+            {
+                Console.Write("Guess a higher number: ");
+            }
+            else if (guess > number)
+            {
+                Console.Write("Guess a lower number: ");
+            }
+            guess = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("Congratulations! You guessed the correct number.");
     }
 }
