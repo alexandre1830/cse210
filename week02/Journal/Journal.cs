e522529e813a8e39026edf1348563d51cf2fd36a
alexandre1830 <alexandre.olv.sz@gmail.com>
2025-11-07 21:44:29 -0300

    Week 02 stubs

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,24 @@
+using System;
+
+class Journal
+{
+    public List<Entry> _entries;
+    public void AddEntry(Entry newEntry)
+    {
+        Console.WriteLine("Hello World! This is the Journal Project.");
+    }
+
+    public void DisplayAll()
+    {
+        Console.WriteLine("Hello World! This is the Journal Project.");
+    }
+
+    public void SaveToFile(string file)
+    {
+        Console.WriteLine("Hello World! This is the Journal Project.");
+    }
+    public void LoadFromFile(string file)
+    {
+        Console.WriteLine("Hello World! This is the Journal Project.");
+    }
+}
