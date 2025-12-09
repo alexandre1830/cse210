996ea6e2ee2e6cbe7e1afa5504930bacf6ecced0
alexandre1830 <alexandre.olv.sz@gmail.com>
2025-12-09 10:01:21 -0300

    Week 07 program

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+namespace ExerciseTracking
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        static void Main(string[] args)
+        {
+            List<Activity> activities = new List<Activity>();
+
+            Running run = new Running("03 Nov 2022", 30, 3.0);
+            activities.Add(run);
+
+            Cycling cycle = new Cycling("04 Nov 2022", 30, 15.0);
+            activities.Add(cycle);
+
+            Swimming swim = new Swimming("05 Nov 2022", 30, 20);
+            activities.Add(swim);
+
+            foreach (Activity activity in activities)
+            {
+                Console.WriteLine(activity.GetSummary());
+            }
+        }
     }
 }
